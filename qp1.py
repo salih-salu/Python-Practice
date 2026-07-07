ce965aacab0578c3bf53d7ba874a9005546dc248
salih-salu <mohammedsalihck2001@gmail.com>
2026-07-07 16:44:11 +0530

    qp1 updated

---
+++ qp1.py
@@ -0,0 +1,7 @@
+# 1. Python Program to Calculate the Area of a Triangle 
+print('find the Area of a Triangle')
+breadth = int(input('enter the breadth: '))
+height = int(input('enter the height: '))
+
+area = (breadth * height) / 2
+print(f'Area of a Triangle is: {area}')
