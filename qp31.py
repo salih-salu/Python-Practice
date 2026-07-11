7dd1aa35435d1394807aeff71f29f4d4c96eadd5
salih-salu <mohammedsalihck2001@gmail.com>
2026-07-11 18:21:14 +0530

    qp31.py updated

---
+++ qp31.py
@@ -0,0 +1,15 @@
+# 31. Check a given number is a perfect number or not( perfect number means the sum of factors equal to the same number 6=1+2+3) 
+
+num = int(input('Enter the number: '))
+
+result = 0
+for i in range(1, num):
+    if num%i == 0:
+        result += i
+if num == result:
+    print(f'Yes, {num} is a perfect number')
+else:
+    print(f'No, {num} is not a perfect number')
+
+
+
