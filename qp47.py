42c82a271bb1f68ac1a21d5602d26e0cd7764a79
salih-salu <mohammedsalihck2001@gmail.com>
2026-07-14 15:42:14 +0530

    qp47.py updated

---
+++ qp47.py
@@ -0,0 +1,13 @@
+# 47. Convert a decimal number to binary 
+
+num = int(input('Enter the number: '))
+
+if num == 0:
+    print('0')
+else:
+    res = ''
+    while num > 0:
+        rem = num%2
+        res = str(rem)+res
+        num = num // 2
+    print(int(res))
