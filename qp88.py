805311bf0aea7dbb6bed910d89447135a1eaac1e
Mohammed Salih C K <mohammedsalihck2001@gmail.com>
2026-08-05 20:58:37 +0530

    qp88.py updated

---
+++ qp88.py
@@ -0,0 +1,10 @@
+# 88. Find the key in a dictionary that corresponding to largest number 
+# Dic={‘a’:1,’b’:4,’c:5} 
+# M=max(dic,key=dic.get) 
+
+
+
+Dic={'a':1,'b':4,'c':5} 
+M=max(Dic,key=Dic.get) 
+print(M)
+
